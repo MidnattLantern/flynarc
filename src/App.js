59759cabc5cb4fb8db60085c18ae6c294ddfec23
Alma Isaksson <isakvent@outlook.com>
2024-05-04 14:59:19 +0000

    pushed navbar to the bottom with proper dimensions

--- src/App.js
+++ src/App.js
@@ -1,48 +1,32 @@
 import React from "react";
 import styles from "./App.module.css";
 import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
 import {Route, Switch} from 'react-router-dom';
 import './api/axiosDefaults';
 import SignUpForm from "./pages/authentication/SignUpForm";
 import SignInForm from "./pages/authentication/SignInForm";
-//import axios from "axios";
-
-// Transfered to CurrentAuthenticationContext.js
-/*
-export const CurrentAuthenticationContext = createContext();
-export const setCurrentAuthenticationContext = createContext();
-*/
 
 function App() {
-// Transfered to CurrentAuthenticationContext.js
-/*
-  const [authentication, setAuthentication] = useState(null)
-  const handleMount = async () => {
-    try {
-      const {data} = await axios.get('dj-rest-auth/user/')
-      setAuthentication(data)
-    } catch(err){
-      console.log(err)
-    }
-  }
-  useEffect(() => {
-    handleMount()
-  }, []);
-  */
 
   return (
-    <div >
-      <NavBar />
-      <Container className={styles.MainView}>
+    <div>
+
+      <div className={styles.MainViewContainer}>
         <Switch>
           <Route exact path="/signin" render={() => <SignInForm /> } />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route path="/" render={() => <h1>Page not found</h1>} />
         </Switch>
-      </Container>
+      </div>
+
+      <div className={styles.NavBarPosition}>
+        <div className={styles.NavBarContainer}>
+          <NavBar/>
+        </div>
+      </div>
+
     </div>
-  );
-}
+  )
+};
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from "react-router-dom";
-//import { CurrentAuthenticationContext } from "../App";
 import { useCurrentAuthentication, useSetCurrentAuthentication } from "../contexts/CurrentAuthenticationContext";
 import axios from "axios";
 
@@ -31,23 +28,13 @@ const NavBar = () => {
     </div>
 
     return (
-        <Navbar expand="md" fixed="top">
-        <Container className={styles.NavBarIsland}>
-
-            <Navbar.Brand>FLYNARC</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className={styles.NavBarGroup}>
-
-                <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="/"><i className="fas fa-home" /> Home</NavLink>
-                <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="https://flynarc-api-824d94b4a80f.herokuapp.com/" target="_blank">API</NavLink>
-                <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="/signin">Sign in</NavLink>
-                <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="/signup">Sign up</NavLink>
-                {currentAuthentication ? authenticatedOptions : nonAuthenticatedOptions}
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <Nav>
+            <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="/"><i className="fas fa-home" /> Home</NavLink>
+            <a className={styles.NavBarButton} href="https://flynarc-api-824d94b4a80f.herokuapp.com/" target="blank">API</a>
+            <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="/signin">Sign in</NavLink>
+            <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="/signup">Sign up</NavLink>
+            {currentAuthentication ? authenticatedOptions : nonAuthenticatedOptions}
+        </Nav>
     )
 };
 
