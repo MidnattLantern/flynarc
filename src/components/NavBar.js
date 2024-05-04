b0e8b9435a1fc444b1b683b424d7187106a922e0
Alma Isaksson <isakvent@outlook.com>
2024-05-04 10:07:25 +0000

    add signout button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,19 +5,30 @@ import Navbar from 'react-bootstrap/Navbar';
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from "react-router-dom";
 import { CurrentAuthenticationContext } from "../App";
-import { useCurrentAuthentication } from "../contexts/CurrentAuthenticationContext";
+import { useCurrentAuthentication, useSetCurrentAuthentication } from "../contexts/CurrentAuthenticationContext";
+import axios from "axios";
 
 const NavBar = () => {
-    const CurrentAuthentication = useCurrentAuthentication();
+    const currentAuthentication = useCurrentAuthentication();
+    const setCurrentAuthentication = useSetCurrentAuthentication();
 
     const nonAuthenticatedOptions = <div>
         <p>not authenticated</p>
     </div>
 
-    const authenticatedOptions = <div>
-        <p>authenticated as: {CurrentAuthentication?.username}</p>
-    </div>
+    const handleSignOut = async () => {
+        try {
+            await axios.post("dj-rest-auth/logout/");
+            setCurrentAuthentication(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
+    const authenticatedOptions = <div>
+        <p>authenticated as: {currentAuthentication?.username}</p>
+        <button onClick={handleSignOut}>Sign out</button>
+    </div>
 
     return (
         <Navbar expand="md" fixed="top">
@@ -32,7 +43,7 @@ const NavBar = () => {
                 <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="https://flynarc-api-824d94b4a80f.herokuapp.com/" target="_blank">API</NavLink>
                 <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="/signin">Sign in</NavLink>
                 <NavLink className={styles.NavBarButton} exact activeClassName={styles.Active} to="/signup">Sign up</NavLink>
-                {CurrentAuthentication ? authenticatedOptions : nonAuthenticatedOptions}
+                {currentAuthentication ? authenticatedOptions : nonAuthenticatedOptions}
             </Nav>
             </Navbar.Collapse>
         </Container>
